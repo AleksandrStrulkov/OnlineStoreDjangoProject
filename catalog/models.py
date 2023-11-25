fa7dc724194c008bd0964bd40e3b20ea3e560985
AleksandrStrulkov <astrulkov@yandex.ru>
2023-11-26 00:22:11 +0300

    add task3

--- catalog/models.py
+++ catalog/models.py
@@ -5,9 +5,16 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
-	pass
+	name_product = models.CharField(max_length=100, verbose_name='Наименование')
+	description_product = models.TextField(verbose_name='Описание')
+	image = models.ImageField(upload_to='products/', verbose_name='Изображение')
+	category = models.CharField(max_length=100, verbose_name='Категория')
+	price = models.IntegerField()
+	created_at = models.DateField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 
 class Category(models.Model):
-	pass
+	name_category = models.CharField(max_length=100, verbose_name='Категория')
+	description_category = models.TextField('Описание')
 
